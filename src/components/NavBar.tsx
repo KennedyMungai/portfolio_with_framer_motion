d2ea09a017c4e967374117d5c92ccad602af290c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 15:58:56 +0300

    Made NavBar a client component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -8,7 +9,11 @@ const NavBar = (props: Props) => {
 	return (
 		<nav className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-10 bg-bodyColor px-4'>
 			<div className='max-w-container h-full mx-auto py-1 flex items-center justify-between'>
-				<motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration:0.5}}>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 0.5 }}
+				>
 					<Image
 						src={'/images/logo.png'}
 						width={80}
