d081a04f1596684746b36ba75a0f4802483f0e57
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 15:41:05 +0300

    Added the logo to the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
+
 type Props = {}
 
 const NavBar = (props: Props) => {
 	return (
 		<div className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-10 bg-bodyColor px-4'>
-			NavBar
+			<div className='max-w-container h-full mx-auto py-1 flex items-center justify-between'>
+				<div className=''>
+					<Image
+						src={'/images/logo.png'}
+						width={80}
+						height={80}
+						alt={'Logo'}
+						className='rounded-full'
+					/>
+				</div>
+				<div className=''></div>
+			</div>
 		</div>
 	)
 }
