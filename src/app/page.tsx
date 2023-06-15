d0b443ce4692f85f445bccc84e0f1befd8c5568b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 15:10:59 +0300

    Rendered out the NavBar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import NavBar from '@/components/NavBar'
 import Image from 'next/image'
 
 export default function Home() {
-	return <main></main>
+	return (
+		<main>
+			<NavBar />
+		</main>
+	)
 }
