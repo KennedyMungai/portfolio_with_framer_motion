9a494eae18c4e221d7d589d1ddef1bbe01195f35
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 15:01:10 +0300

    Deleted the tailwind extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,7 @@ module.exports = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
 	],
 	theme: {
-		extend: {
-			fontFamily: {
-				bodyFont: ['Roboto', 'sans-serif'],
-				titleFont: ['Montserrat', 'sans-serif']
-			}
-		}
+		extend: {}
 	},
 	plugins: []
 }
