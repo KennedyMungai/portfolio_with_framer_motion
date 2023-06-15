389560937bbd21bce6e58a09331315a28300b747
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 15:28:41 +0300

    Defined the NavBar for the app

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,11 @@
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <div>NavBar</div>
+	return (
+		<div className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-10 bg-bodyColor px-4'>
+			NavBar
+		</div>
+	)
 }
 
 export default NavBar
