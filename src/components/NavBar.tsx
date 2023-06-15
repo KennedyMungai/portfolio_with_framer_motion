9adf5e8f1409d6044668c4f05cb186be44ce9b95
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 16:34:05 +0300

    Styled a simple spam

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -101,7 +101,9 @@ const NavBar = (props: Props) => {
 					</Link>
 				</div>
 				{/* Small Icon Section */}
-				<div className='w-6 h-5 flex flex-col justify-between items-center md:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group'></div>
+				<div className='w-6 h-5 flex flex-col justify-between items-center md:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group'>
+					<span className='w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300'></span>
+				</div>
 			</div>
 		</nav>
 	)
