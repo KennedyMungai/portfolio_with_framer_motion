bbc70500b4fd8cb864e16df54a099eb4d6a614d0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 08:22:57 +0300

    Added a bunch of dividers for the meat of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
 	return (
 		<main className='w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll'>
 			<NavBar />
+			<div className='w-full h-[88vh] xl:flex items-center gap-20 justify-between'>
+				<div className=''></div>
+				<div className=''></div>
+				<div className=''></div>
+			</div>
 		</main>
 	)
 }
