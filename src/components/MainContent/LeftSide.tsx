ed238dc8000cc12aa6d5ede244cb7abd79a23175
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 08:39:07 +0300

    Added some css classes to make the icons catchier when hovering over them

--- src/components/MainContent/LeftSide.tsx
+++ src/components/MainContent/LeftSide.tsx
@@ -12,12 +12,12 @@ type Props = {}
 const LeftSide = (props: Props) => {
 	return (
 		<div className='w-full h-full flex flex-col items-center justify-end gap-4 text-textLight'>
-			<div className=''>
+			<div className='flex flex-col items-center gap-4'>
 				<Link
 					href='https://www.github.com/KennedyMungai'
 					target='_blank'
 				>
-					<span>
+					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300'>
 						<TbBrandGithub size={30} />
 					</span>
 				</Link>
