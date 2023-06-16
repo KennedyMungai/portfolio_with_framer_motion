88b4d8a7bb1fb6bf3dfee703c285d4fccfd589ec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 10:17:56 +0300

    CHanged the initial y position and added a delay to the transition object

--- src/components/MainContent/Middle.tsx
+++ src/components/MainContent/Middle.tsx
@@ -9,9 +9,14 @@ const Middle = (props: Props) => {
 			id='Home'
 			className='max-w-contentContainer mx-auto py-10 md:py-24 flex flex-col gap-4 lg:gap-8 xl:px-4'
 		>
-			<h3 className='text-textGreen text-lg tracking-wide'>
+			<motion.h3
+				initial={{ y: 10, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				transition={{ duration: 0.5, delay: 0.6 }}
+				className='text-textGreen text-lg tracking-wide'
+			>
 				Hi, my name is
-			</h3>
+			</motion.h3>
 			<h1 className=''>
 				Kennedy Mungai.<span> I build things for the web</span>
 			</h1>
