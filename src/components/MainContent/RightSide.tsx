a084bd7b4724f5be5a6d9c69154e90aa31465474
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 09:46:39 +0300

    Updateed the transition duration to 1.5s

--- src/components/MainContent/RightSide.tsx
+++ src/components/MainContent/RightSide.tsx
@@ -7,14 +7,19 @@ type Props = {}
 
 const RightSide = (props: Props) => {
 	return (
-		<div className='w-full h-full flex flex-col items-center justify-end gap-6 text-textLight'>
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 1.5 }}
+			className='w-full h-full flex flex-col items-center justify-end gap-6 text-textLight'
+		>
 			<Link href='mailto:kennedymungaifmab@gmail.com'>
 				<p className='text-sm rotate-90 w-72 tracking-wide text-textGreen'>
 					My Email Address
 				</p>
 			</Link>
 			<span className='w-[2px] h-32 bg-textGreen inline-flex'></span>
-		</div>
+		</motion.div>
 	)
 }
 
