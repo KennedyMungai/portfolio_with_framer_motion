7c7f71a2790c2ec8ced6fedb1e8c25038469ae0e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 11:57:29 +0300

    Added a span line

--- src/components/MainContent/SectionTitle.tsx
+++ src/components/MainContent/SectionTitle.tsx
@@ -3,8 +3,11 @@ type Props = {}
 const SectionTitle = (props: Props) => {
 	return (
 		<h2 className='text-2xl font-semibold flex flex-row items-center'>
-			<span className='text-base md:text-lg text-textGreen mr-2'>01. </span>
+			<span className='text-base md:text-lg text-textGreen mr-2'>
+				01.{' '}
+			</span>
 			About Me
+			<span className='hidden md:inline-flex flex md:w-60 lg:w-72 h-[.5px] bg-gray-700 ml-6'></span>
 		</h2>
 	)
 }
