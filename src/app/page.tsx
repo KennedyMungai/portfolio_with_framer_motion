93e7ec01f50f5607cecd0e2a2549f6d12d2f32cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 08:24:23 +0300

    Rendered out the left side component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import LeftSide from '@/components/MainContent/LeftSide'
 import NavBar from '@/components/NavBar'
 import Image from 'next/image'
 
@@ -6,7 +7,9 @@ export default function Home() {
 		<main className='w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll'>
 			<NavBar />
 			<div className='w-full h-[88vh] xl:flex items-center gap-20 justify-between'>
-				<div className=''></div>
+				<div className=''>
+					<LeftSide />
+				</div>
 				<div className=''></div>
 				<div className=''></div>
 			</div>
