cad4b136b510d0986e52ac368e5f7265b32fe275
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 09:16:25 +0300

    Styled the div with the social media icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 		<main className='w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll'>
 			<NavBar />
 			<div className='w-full h-[88vh] xl:flex items-center gap-20 justify-between'>
-				<div className=''>
+				<div className='hidden xl:flex w-32 h-full fixed left-0 bottom-0'>
 					<LeftSide />
 				</div>
 				<div className=''></div>
