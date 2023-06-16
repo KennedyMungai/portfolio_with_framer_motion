98aeddd0e60893b7d16212f5403a1108cca737f0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 11:25:25 +0300

    Styled the about section

--- src/components/MainContent/About.tsx
+++ src/components/MainContent/About.tsx
@@ -1,7 +1,14 @@
 type Props = {}
 
 const About = (props: Props) => {
-	return <section>About</section>
+	return (
+		<section
+			id='about'
+			className='max-w-containerSmall mx-auto py-10 lg:py-32 flex flex-col gap-8'
+		>
+			About
+		</section>
+	)
 }
 
 export default About
