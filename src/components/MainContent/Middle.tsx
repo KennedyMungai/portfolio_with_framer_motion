4e182e3db1c9b30ee76a85dac3052a055166d8d9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 09:56:14 +0300

    Added some styles to the middle section

--- src/components/MainContent/Middle.tsx
+++ src/components/MainContent/Middle.tsx
@@ -2,7 +2,14 @@
 type Props = {}
 
 const Middle = (props: Props) => {
-	return <section id='Home'>Middle</section>
+	return (
+		<section
+			id='Home'
+			className='max-w-contentContainer mx-auto py-10 md:py-24 flex flex-col gap-4 lg:gap-8 xl:px-4'
+		>
+			Middle
+		</section>
+	)
 }
 
 export default Middle
