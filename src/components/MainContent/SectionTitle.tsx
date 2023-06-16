a0f52aadc6eff75a620ef849479c72af524eb348
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 12:02:54 +0300

    Made some simple adjustments

--- src/components/MainContent/SectionTitle.tsx
+++ src/components/MainContent/SectionTitle.tsx
@@ -7,7 +7,7 @@ const SectionTitle = ({title_number, title}: Props) => {
 	return (
 		<h2 className='text-2xl font-semibold flex flex-row items-center'>
 			<span className='text-base md:text-lg text-textGreen mr-2'>
-				{title_number + ' '}
+				{title_number + '. '}
 			</span>
 			{title}
 			<span className='hidden md:inline-flex md:w-60 lg:w-72 h-[.5px] bg-gray-700 ml-6'></span>
