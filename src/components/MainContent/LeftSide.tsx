b13f465d33ff9c83f9daf039757e6f378a342c58
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 08:31:28 +0300

    Added some styles to the left side div

--- src/components/MainContent/LeftSide.tsx
+++ src/components/MainContent/LeftSide.tsx
@@ -2,14 +2,18 @@ import { TbBrandGithub } from 'react-icons/tb'
 import {
 	SlSocialYoutube,
 	SlSocialFacebook,
-	SlSocialLinkedIn,
+	SlSocialLinkedin,
 	SlSocialInstagram
 } from 'react-icons/sl'
 
 type Props = {}
 
 const LeftSide = (props: Props) => {
-	return <div>LeftSide</div>
+    return (
+		<div className='w-full h-full flex flex-col items-center justify-end gap-4 text-textLight'>
+			Left Side
+		</div>
+	)
 }
 
 export default LeftSide
