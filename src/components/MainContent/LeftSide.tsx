60137702c0fb755fc9937ce7c2caca8781fa95f3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 08:43:50 +0300

    Added icons for the other social media links

--- src/components/MainContent/LeftSide.tsx
+++ src/components/MainContent/LeftSide.tsx
@@ -20,6 +20,18 @@ const LeftSide = (props: Props) => {
 					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
 						<TbBrandGithub size={30} />
 					</span>
+					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+						<SlSocialYoutube size={30} />
+					</span>
+					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+						<SlSocialFacebook size={30} />
+					</span>
+					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+						<SlSocialLinkedin size={30} />
+					</span>
+					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+						<SlSocialInstagram size={30} />
+					</span>
 				</Link>
 			</div>
 		</div>
