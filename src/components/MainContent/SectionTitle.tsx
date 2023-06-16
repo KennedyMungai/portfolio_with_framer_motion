432407fa884ebb700c669e5b5d29735a6c1deddb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 11:55:34 +0300

    Styled the items

--- src/components/MainContent/SectionTitle.tsx
+++ src/components/MainContent/SectionTitle.tsx
@@ -2,10 +2,8 @@ type Props = {}
 
 const SectionTitle = (props: Props) => {
 	return (
-		<h2>
-			<span className='text-2xl font-semibold flex flex-row items-center text-textGreen'>
-				01.{' '}
-			</span>
+		<h2 className='text-2xl font-semibold flex flex-row items-center'>
+			<span className='text-base md:text-lg text-textGreen mr-2'>01. </span>
 			About Me
 		</h2>
 	)
