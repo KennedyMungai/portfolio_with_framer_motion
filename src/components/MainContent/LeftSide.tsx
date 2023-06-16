113ca910938c6f91fff4649d03950d75e2d2d79e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-16 09:03:47 +0300

    Updated the markup for the social media links

--- src/components/MainContent/LeftSide.tsx
+++ src/components/MainContent/LeftSide.tsx
@@ -1,18 +1,18 @@
-import { TbBrandGithub } from 'react-icons/tb'
-import {
-	SlSocialYoutube,
-	SlSocialFacebook,
-	SlSocialLinkedin,
-	SlSocialInstagram
-} from 'react-icons/sl'
 import Link from 'next/link'
+import {
+	SlSocialFacebook,
+	SlSocialInstagram,
+	SlSocialLinkedin,
+	SlSocialYoutube
+} from 'react-icons/sl'
+import { TbBrandGithub } from 'react-icons/tb'
 
 type Props = {}
 
 const LeftSide = (props: Props) => {
 	return (
 		<div className='w-full h-full flex flex-col items-center justify-end gap-4 text-textLight'>
-			<div className='flex flex-col items-center gap-4'>
+			<div className='flex flex-col items-center justify-center gap-4 text-textLight'>
 				<Link
 					href='https://www.github.com/KennedyMungai'
 					target='_blank'
@@ -20,15 +20,23 @@ const LeftSide = (props: Props) => {
 					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
 						<TbBrandGithub size={30} />
 					</span>
+				</Link>
+				<Link href='https://https.www.youtube.com/channel/KennedyMungai'>
 					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
 						<SlSocialYoutube size={30} />
 					</span>
+				</Link>
+				<Link href='https://www.facebook.com/kennedy.mungai.9'>
 					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
 						<SlSocialFacebook size={30} />
 					</span>
+				</Link>
+				<Link href='https://www.linkedin.com/in/kennedy-mungai-a8b5b917b'>
 					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
 						<SlSocialLinkedin size={30} />
 					</span>
+				</Link>
+				<Link href='https://www.instagram.com/kennedy_mungai'>
 					<span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>
 						<SlSocialInstagram size={30} />
 					</span>
